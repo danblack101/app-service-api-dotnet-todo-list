9c1c022976066b1865802b22584f2c9cb06d1d18
Dan Black <danblack101@gmail.com>
2016-12-29 12:18:23 +1300

    Add NoteController

---
+++ src/ToDoListDataAPI/Controllers/NoteController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ToDoListDataAPI.Controllers
+{
+    public class NoteController : ApiController
+    {
+
+        public IEnumerable<Note> Get()
+        {
+            var list = new List<Note>()
+            {
+                new Note {content = "Hello World" }
+            };
+
+            return list;
+        }
+    }
+
+    public class Note
+    {
+        public string content { get; set; }
+    }
+}
